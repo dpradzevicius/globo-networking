bd7ae54cdf47e95eb294ed6691dfa90997c52dda
Darius Pradzevicius <darius.pradzevicius@macaw.nl>
2025-05-20 15:55:04 +0300

    Initial commit

---
+++ imports.tf
@@ -0,0 +1,48 @@
+# ##################################################################################
+# # IMPORTS
+# ##################################################################################
+
+# import {
+#   to = module.avm-res-network-virtualnetwork.azapi_resource.vnet
+#   id = "/subscriptions/11a0f549-fda9-4b8c-bce0-362e39848d1e/resourceGroups/learning02/providers/Microsoft.Network/virtualNetworks/globo-dev" #VPC
+# }
+
+# import {
+#   to = module.avm-res-network-virtualnetwork.module.subnet["public-1"].azapi_resource.subnet
+#   id = "/subscriptions/11a0f549-fda9-4b8c-bce0-362e39848d1e/resourceGroups/learning02/providers/Microsoft.Network/virtualNetworks/globo-dev/subnets/globo-dev-public-1"
+# }
+
+# import {
+#   to = module.avm-res-network-virtualnetwork.module.subnet["public-2"].azapi_resource.subnet
+#   id = "/subscriptions/11a0f549-fda9-4b8c-bce0-362e39848d1e/resourceGroups/learning02/providers/Microsoft.Network/virtualNetworks/globo-dev/subnets/globo-dev-public-2"
+# }
+
+# # import {
+# #   to = module.main.aws_internet_gateway.this[0]
+# #   id = "InternetGateway" #InternetGateway
+# # }
+
+# # import {
+# #   to = module.main.aws_route.public_internet_gateway[0]
+# #   id = "DefaultPublicRoute" #DefaultPublicRoute
+# # }
+
+# # import {
+# #   to = module.main.aws_route_table.public[0]
+# #   id = "PublicRouteTable" #PublicRouteTable
+# # }
+
+# # import {
+# #   to = module.main.aws_route_table_association.public[0]
+# #   id = "PublicSubnet1/PublicRouteTable" #PublicSubnet1/PublicRouteTable
+# # }
+
+# # import {
+# #   to = module.main.aws_route_table_association.public[1]
+# #   id = "PublicSubnet2/PublicRouteTable" #PublicSubnet2/PublicRouteTable
+# # }
+
+# # import {
+# #   to = aws_security_group.ingress
+# #   id = "NoIngressSecurityGroup" #NoIngressSecurityGroup
+# # }
---
+++ terraform.tf
@@ -0,0 +1,33 @@
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~>3.0.0"
+#     }
+#   }
+# }
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    azapi = {
+      source  = "azure/azapi"
+    }
+    modtm = {
+      source = "azure/modtm"
+    }
+  }
+
+  required_version = ">= 1.5"
+}
